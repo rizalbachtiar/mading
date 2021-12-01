2db7883b4bf8fdb5caf34d0ec58728560e12be82
rizal bachtiar <rizal.bachtiar.31@gmail.com>
2021-12-01 23:47:46 +0700

    membuat tampilan list data mading

--- resources/js/Pages/MadingIndex.vue
+++ resources/js/Pages/MadingIndex.vue
@@ -1,10 +1,30 @@
 <template>
-	 <div>
-	 	<ul>
-	 		<li v-for="mading in madings">
-	 			{{ mading.name }} - <Link :title="mading.name" class="text-blue-700" :href="route('madings.edit', { mading })">[edit]</Link>
-	 		</li>
-	 	</ul>
+	 <div class="flex flex-col items-center py-10">
+	 	<div class="w-8/12 md:w-6/12 bg-blue-500 rounded-3xl px-5 pt-3 pb-5 space-y-3">
+	 		<div class="font-bold text-white text-center">
+	 			List Data Mading
+	 		</div>
+	 		<ul class="space-y-3">
+		 		<li v-for="mading in madings" class="bg-white rounded-full shadow px-4 py-2 flex justify-between">
+		 			<div class="font-semibold text-gray-600">
+		 				{{ mading.name }}
+		 			</div>
+		 			<div class="flex items-center space-x-1">
+		 				<Link :title="mading.name" class="" :href="route('madings.edit', { mading })">
+		 					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600 hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+							  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+							</svg>
+		 				</Link>
+		 				<Link :title="mading.name" class="" :href="route('madings.edit', { mading })">
+		 					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600 hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+							</svg>
+		 				</Link>
+		 			</div>
+		 		</li>
+		 	</ul>
+	 	</div>
 	 </div>
 </template>
 <script>
