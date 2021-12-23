93291192f053dd4e21df0e1483f2db28879d69f1
rizal bachtiar <rizal.bachtiar.31@gmail.com>
2021-12-24 00:06:37 +0700

    melanjutkan tampilan detail

--- resources/js/Pages/DetailMagazine.vue
+++ resources/js/Pages/DetailMagazine.vue
@@ -1,10 +1,16 @@
 <template>
 	<div class="flex flex-col max-w-6xl bg-green-100 mx-auto space-y-2 pt-5">
 		<div class="text-gray-600 text-3xl font-bold text-center">Judul</div>
-		<div class="flex">
-			<div class="w-2/12 bg-red-300"> Rizal Bachtiar</div>
-			<div class="w-6/12 bg-blue-300"> Rizal Bachtiar</div>
-			<div class="w-4/12 bg-red-300"> Rizal Bachtiar</div>
+		<div class="flex space-x-2">
+			<div class="w-2/12 bg-red-300 rounded-md px-2 py-3">
+				<div class="text-center text-xl">Content</div>
+				<ul class="">
+					<li><a href="#">Puisi</a></li>
+					<li><a href="#">Cerpen</a></li>
+				</ul>
+			</div>
+			<div class="w-6/12 bg-blue-300">Mading</div>
+			<div class="w-4/12 bg-red-300">Galery</div>
 		</div>
 	</div>
 </template>
