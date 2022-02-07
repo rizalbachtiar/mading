5afa0b431f876a83ff3168a4ad1116e5d5a8e862
rizalbachtiar <rizal.bachtiar.31@gmail.com>
2022-02-07 23:53:29 +0700

    menambah field type pada tabel pages

--- resources/js/Pages/DetailMagazine.vue
+++ resources/js/Pages/DetailMagazine.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="h-screen w-screen bg-cover overflow-auto" style="background-image:url('/images/bg-magazine.jpg')">
         <div class="flex flex-col lg:max-w-5xl md:max-w-2xl mx-auto pt-5 px-4 space-y-5">
-            <div class="text-center text-2xl font-bold text-gray-700">{{ magazine.name }}</div>
+            <div class="text-center text-4xl font-bold text-gray-700">{{ magazine.name }}</div>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
               <Link v-for="page in pages" :href="route('home.detail-content')" class="bg-white bg-opacity-30 hover:bg-opacity-60 backdrop-filter backdrop-blur py-5 px-2 rounded-lg">
-                  <p class="text-gray-700 text-xl font-bold text-center">{{ page.title }}</p>
+                  <p class="text-gray-700 text-2xl font-bold text-center">{{ page.title }}</p>
               </Link>
             </div>
         </div>
